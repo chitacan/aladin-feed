b018cc6c1573047afc9b360b99e140df0d1fea47
Kyungyeol Kim <chitacan@gmail.com>
2025-01-19 19:02:54 +0900

    Support login with captcha

--- main.ts
+++ main.ts
@@ -1,14 +1,21 @@
 import { Feed } from "npm:feed@4";
-import { cheerio } from "https://deno.land/x/cheerio@1.0.7/mod.ts";
-import { parse } from "https://deno.land/std@0.179.0/flags/mod.ts";
+import { DOMParser, Element } from "jsr:@b-fuze/deno-dom";
+import { parseArgs } from "jsr:@std/cli/parse-args";
+import { Jimp } from "npm:jimp";
 
-const args = parse(Deno.args);
+const BASE_URL = "https://www.aladin.co.kr";
+const USER_AGENT =
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36";
+
+const flags = parseArgs(Deno.args, {
+  boolean: ["help"],
+});
 
 if (!import.meta.main) {
   Deno.exit(0);
 }
 
-if (args["help"]) {
+if (flags.help) {
   console.log(`
   Usage
     $ deno run -A main.ts
@@ -16,97 +23,205 @@ if (args["help"]) {
   Deno.exit(0);
 }
 
-const email = Deno.env.get('ALADIN_EMAIL');
-const password = Deno.env.get('ALADIN_PASSWORD');
+const EMAIL = Deno.env.get("ALADIN_EMAIL");
+const PASSWORD = Deno.env.get("ALADIN_PASSWORD");
+const OPENAI_API_KEY = Deno.env.get("OPENAI_API_KEY");
 
-if (!email || !password) {
-  console.log('ALADIN_EMAIL and ALADIN_PASSWORD environment variables are required.');
+if (!EMAIL || !PASSWORD || !OPENAI_API_KEY) {
+  console.log("Cannot find required environment variables.");
   Deno.exit(1);
 }
 
-const formData = new URLSearchParams({
-  Email: email,
-  Password: password,
-  Action: '1',
-  ReturnUrl: '',
+async function readCaptcha(): Promise<
+  {
+    cookies: string[];
+    body: { txtCaptcha: string } | Record<string | number | symbol, never>;
+  }
+> {
+  const { html: loginPopup, cookies } = await fetch(
+    `${BASE_URL}/login/wlogin.aspx?returnurl=/`,
+    {
+      headers: {
+        "User-Agent": USER_AGENT,
+      },
+    },
+  )
+    .then(async (res) => {
+      const cookies = res.headers.getSetCookie().map((d) => d.split(";")[0]);
+      const html = await res.text();
+      return { html, cookies };
+    });
+
+  const loginPopup$ = new DOMParser()
+    .parseFromString(loginPopup, "text/html");
+  const captcha = loginPopup$.querySelector("#imgCaptcha");
+
+  if (!captcha) {
+    return { cookies, body: {} };
+  }
+
+  const now = new Date();
+  const imageURL = encodeURI(
+    `${BASE_URL}/ucl/aladdin/captcha.ashx?x=${now.toUTCString()}`,
+  );
+
+  const buffer = await fetch(imageURL, {
+    credentials: "include",
+    headers: {
+      "Accept": "image/*",
+      Cookie: cookies.join("; "),
+    },
+  }).then(res => res.arrayBuffer());
+
+  const img = await Jimp.read(buffer);
+  const dataURL = await img.getBase64("image/png");
+  const png = await img.getBuffer("image/png");
+
+  // write dataURL to file
+  await Deno.writeFile("captcha.png", png);
+
+  const result = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-4o",
+      messages: [
+        {
+          "role": "user",
+          "content": [
+            {
+              "type": "text",
+              "text":
+                "What is the 4 numbers in this image? Please include only 4 numbers in your response.",
+            },
+            {
+              "type": "image_url",
+              "image_url": {
+                "url": dataURL,
+              },
+            },
+          ],
+        },
+      ],
+    }),
+  })
+    .then((res) => res.json());
+
+  const captchaResult = result.choices[0].message.content;
+  console.log("read:", captchaResult);
+  return {
+    cookies,
+    body: { txtCaptcha: captchaResult },
+  };
+}
+
+// get login popup and find if there is captcha
+const captchaResult = await readCaptcha();
+
+console.log("captchaResult", captchaResult.cookies.join("; "), {
+  ...captchaResult.body,
 });
 
-const loginResponse = await fetch('https://www.aladin.co.kr/login/wlogin_popup.aspx', {
-  method: 'POST',
+// await new Promise((resolve) => setTimeout(resolve, 5000));
+
+const loginResponse = await fetch(`${BASE_URL}/login/wlogin.aspx?returnurl=/`, {
+  method: "POST",
+  credentials: "include",
   headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
+    "Content-Type": "application/x-www-form-urlencoded",
+    Cookie: captchaResult.cookies.join("; "),
   },
-  body: formData.toString(),
+  body: new URLSearchParams({
+    Email: EMAIL,
+    Password: PASSWORD,
+    Action: "1",
+    ReturnUrl: "",
+    ...captchaResult.body,
+  }).toString(),
 });
 
 const loginHTML = await loginResponse.text();
+const loginCookies = loginResponse.headers.getSetCookie().map((d) =>
+  d.split(";")[0]
+);
 
-if (!loginHTML.includes('window.location.href')) {
-  throw new Error('Login failed with ' + loginResponse);
+console.log("loginCookies", loginCookies);
+
+const match = loginHTML.match(/<script>alert\(\"(.*)\"\)/);
+if (match) {
+  throw new Error(`Login failed with: ${match[1]}`);
 }
 
-const [user, session, us, login] = loginResponse.headers.getSetCookie()
-  .map(d => d.split(';')[0]);
+if (!loginHTML.includes("document.location.href")) {
+  throw new Error("Login failed with redirections");
+}
 
-const basketResponse = await fetch('https://aladin.co.kr/shop/wsafebasket.aspx?start=we', {
-  credentials: 'include',
+const basketHTML = await fetch(`${BASE_URL}/shop/wsafebasket.aspx?start=we`, {
+  credentials: "include",
   headers: {
-    Cookie: `${user}; ${session}; ${us}; ${login}; ordersavebasket=ViewType=Simple&ViewRowsCount=96`,
-  }
-});
+    Cookie: [
+      ...captchaResult.cookies,
+      ...loginCookies,
+      "ordersavebasket=ViewType=Simple&ViewRowsCount=96",
+    ].join("; "),
+  },
+})
+  .then((res) => res.text());
 
-const basketHTML = await basketResponse.text();
-const $ = cheerio.load(basketHTML);
-const books$ = $('#Myform > *:nth-child(2) > table > * > * > td tr:nth-child(2)');
-
-const books = books$.map((_i, el) => {
-  const book$ = $(el);
-  const title = book$.find('a').text();
-  const link = book$.find('a').attr('href')!;
-  const id = new URL(link || '').searchParams.get('ItemId')!;
-  const rawPrice = book$.find('.ss_p').first().text();
-  const price = parseInt(rawPrice.replace(/,/g, ''));
-  const date = book$.find('.fontcolor_gray').text()
-    .replace(/(일에 보관$|\s)/g, '')
-    .replace(/\D/g, '-');
-  const coverImage = book$.prev().find('img').attr('src');
-
-  return {
-    id,
-    guid: id,
-    title,
-    link,
-    description: `![](${coverImage})
-${rawPrice}원`,
-    content: JSON.stringify({
-      price,
-      coverImage,
-    }),
-    author: [{name: 'chitacan', link: 'https://github.com/chitacan'}],
-    date: new Date(date),
-  };
-});
+const basket$ = new DOMParser().parseFromString(basketHTML, "text/html");
+const selector =
+  "#Myform > *:nth-child(2) > table > * > * > td tr:nth-child(2)";
+const books = Array.from(basket$.querySelectorAll(selector))
+  .map((bookHTML) => {
+    const title = bookHTML.querySelector("a")!.textContent;
+    const link = bookHTML.querySelector("a")!.getAttribute("href")!;
+    const id = new URL(link || "").searchParams.get("ItemId")!;
+    const rawPrice = bookHTML.querySelector(".ss_p")?.textContent;
+    const price = parseInt(rawPrice?.replace(/,/g, "") || "0");
+    const date = bookHTML.querySelector(".fontcolor_gray")!.textContent
+      .replace(/(일에 보관$|\s)/g, "")
+      .replace(/\D/g, "-");
+    const coverImage = (bookHTML.previousSibling as Element)
+      ?.querySelector("img")?.getAttribute("src");
+    return {
+      id,
+      guid: id,
+      title,
+      link,
+      description: `![](${coverImage})
+  ${rawPrice}원`,
+      content: JSON.stringify({
+        price,
+        coverImage,
+      }),
+      author: [{ name: "chitacan", link: "https://github.com/chitacan" }],
+      date: new Date(date),
+    };
+  });
 
 const feed = new Feed({
-  title: 'aladin basket',
+  title: "aladin basket",
   description: "chitacan's personal dogdrip feed",
-  id: 'aladin',
-  link: 'https://www.aladin.co.kr/shop/wsafebasket.aspx',
+  id: "aladin",
+  link: "https://www.aladin.co.kr/shop/wsafebasket.aspx",
   image: "https://image.aladin.co.kr/img/logo_big.jpg",
-  favicon: 'https://image.aladin.co.kr/img/home/aladin.ico',
-  copyright: 'All rights reserved 2024, chiatacn',
+  favicon: "https://image.aladin.co.kr/img/home/aladin.ico",
+  copyright: "All rights reserved 2025, chiatacn",
   updated: new Date(),
   feedLinks: {
-    json: ""
+    json: "",
   },
   author: {
     name: "chitacan",
-    link: "https://github.com/chitacan"
-  }
+    link: "https://github.com/chitacan",
+  },
 });
 
-for(const book of books) {
+for (const book of books) {
   feed.addItem(book);
 }
 
-await Deno.writeTextFile('feed.xml', feed.rss2());
+await Deno.writeTextFile("feed.xml", feed.rss2());
